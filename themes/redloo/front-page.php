a41508d08849a0c293e7f58e4e901fdf486cc924
hiroshi0122 <hiroshi.ihara@nanofun.co.jp>
2025-11-12 13:37:39 +0900

    footer以外のTOPコーディング完了

--- themes/redloo/front-page.php
+++ themes/redloo/front-page.php
@@ -354,35 +354,117 @@ get_header();
         </div>
     </div>
 </section>
+<?php
+    get_template_part('template-parts/components/marquee', null, [
+    'text' => 'MEASURED AT 36 POINTS.',
+    'modifier' => ''
+    ]);
+?>
 
 
-<?php // NEWS SECTION // *********************************************************** // ?>
-<section class="news-sec">
+<?php // BLOG SECTION // *********************************************************** // ?>
+<section class="blog-sec">
     <div class="container">
-        <div class="row">
-            <div class="col-md-4">
-                <div class="main-title blur">
-                    <span>NEWS</span>
-                    <h2 class="daily-title">お知らせ</h2>
+        <div class="bg-gray">
+            <div class="title-template mb-md-10 text-center">
+                <div class="title">
+                    <span>お知らせ & お役立ち情報</span>
+                    <h2>BLOG</h2>
                 </div>
             </div>
-            <div class="col-md-8">
-                <?php if ($posts->have_posts()) : ?>
-                    <?php while ($posts->have_posts()) : $posts->the_post(); ?>
+            
+            <?php get_template_part('template-parts/components/news-list'); ?>
 
-                        <?php get_template_part('template-parts/components/news-list'); ?>
-
-                    <?php endwhile; ?>
-                <?php endif; ?>
-
-                <div class="btn-area justify-content-end pt-5">
-                    <a class="btn-more" href="/news">More >>></a>
-                </div>
+            <div class="btn-area justify-center pt-8">
+                <a class="btn-black" href="/blog">ブログ一覧</a>
             </div>
         </div>
     </div>
 </section>
+<?php
+    get_template_part('template-parts/components/marquee', null, [
+    'text' => 'OUR INFORATION & NEWS!',
+    'modifier' => 'reverse'
+    ]);
+?>
 
+
+
+<?php // ACCESS SECTION // *********************************************************** // ?>
+<section class="access-sec" id="accessSec">
+    <div class="container">
+        <div class="title-template mb-md-10 text-center">
+            <div class="title">
+                <span>アクセス</span>
+                <h2>ACCESS</h2>
+                <h4 class="catch blue">スタッフの準備等ございますため、<br>ご来訪の際は、事前にご予約をお願いいたします。</h4>
+                <span class="pink">※対応スタッフが不在の場合があります。</span>
+            </div>
+        </div>
+
+        <div class="about-visit text-center">
+            <div class="border-title center">
+                <h3>訪問・採寸について</h3>
+            </div>
+            <p>無料採寸やオーダーのご相談は、下記の2拠点にて対応しています。 <br>最寄駅は、つくばエクスプレス「六町駅」（北千住から2駅）です。<br>採寸のみをご希望の方は、株式会社トミテック本社でも承ります。</p>
+        </div>
+
+        <div class="access-info">
+            <div class="row gap-y-10 gap-md-10 mb-md-15">
+                <div class="col-12 col-md-6">
+                    <ul class="labels mb-5">
+                        <li><span class="label">採寸のみ</span></li>
+                    </ul>
+                    <div class="logo">
+                        <img src="<?php echo get_template_directory_uri(); ?>/assets/images/common/tomitech_logo.webp" alt="株式会社トミテックロゴ">
+                    </div>
+                    <ul class="access-info-detail">
+                        <li class="address">東京都足立区六町4丁目12番15号</li>
+                        <li class="train">つくばエクスプレス「六町駅」から徒歩約5分</li>
+                        <li class="car">首都高速6号三郷線「加平出口」より約10分</li>
+                    </ul>
+                    <div class="google-map">
+                        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3236.574958221522!2d139.82359367728276!3d35.7858158725538!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x601890265315f099%3A0x8c7d72420390d989!2z44ix44OI44Of44OG44OD44Kv!5e0!3m2!1sja!2sjp!4v1762917277611!5m2!1sja!2sjp" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                        <a class="btn-map" href="https://maps.app.goo.gl/uNWm6BLGQNec7UR46" target="_blank">Goole Maps</a>
+                    </div>
+                </div>
+                <div class="col-12 col-md-6">
+                    <div class="d-flex align-center gap-4">
+                        <div class="logo redloo">
+                            <img src="<?php echo get_template_directory_uri(); ?>/assets/images/common/redloo_store_logo.webp" alt="RedLooストアロゴ">
+                        </div>
+                        <div class="store-name">
+                            <ul class="labels">
+                                <li><span class="label">採寸</span></li>
+                                <li><span class="label">サンプル見学</span></li>
+                                <li><span class="label">ご相談</span></li>
+                            </ul>
+                            <div class="border-title">
+                                <h5>RedLoo (縫製工場)</h5>
+                            </div>
+                        </div>
+                    </div>
+                    <ul class="access-info-detail">
+                        <li class="address">東京都足立区東保木間1丁目3番2号</li>
+                        <li class="train">つくばエクスプレス「六町駅」から徒歩約15分</li>
+                        <li class="car">首都高速6号三郷線「加平出口」より約15分</li>
+                    </ul>
+                    <div class="google-map">
+                        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3236.432473545617!2d139.80820797728293!3d35.7893148725525!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x601891cc508ecf03%3A0xb113b85bdadf1062!2z44CSMTIxLTAwNjMg5p2x5Lqs6YO96Laz56uL5Yy65p2x5L-d5pyo6ZaT77yR5LiB55uu77yT4oiS77yS!5e0!3m2!1sja!2sjp!4v1762917590716!5m2!1sja!2sjp" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                        <a class="btn-map" href="https://maps.app.goo.gl/uWy4JEw7QbHRYKxP6" target="_blank">Goole Maps</a>
+                    </div>
+                </div>
+            </div>
+            <div class="tel-info">
+                <div class="border-title">
+                    <h3>お電話でのお問合わせ</h3>
+                </div>
+                <h5 class="tel">03-1234-5678</h5>
+            </div>
+
+        </div>
+    </div>
+</section>
 
 <?php // CONTACT SECTION // *********************************************************** // ?>
 <!-- <section class="contact-sec" id="contact">
--- themes/redloo/template-parts/components/news-list.php
+++ themes/redloo/template-parts/components/news-list.php
@@ -1,9 +1,56 @@
-<div class="news-contents">
-    <div class="text-area">
-        <span class="date"><?php echo get_post_time('Y.n.j'); ?></span>
-        <a class="title-link" href="<?php the_permalink(); ?>">
-            <h4><?php the_title(); ?></h4>
-            <?php the_excerpt(); ?>
-        </a>
+<?php
+$args = array(
+    'post_type'      => 'post', // 通常投稿
+    'posts_per_page' => 5,      // 表示件数（調整可）
+    'orderby'        => 'date',
+    'order'          => 'DESC',
+);
+
+$blog_query = new WP_Query($args);
+
+if ($blog_query->have_posts()) :
+?>
+    <div class="news-list">
+        <?php while ($blog_query->have_posts()) : $blog_query->the_post(); ?>
+            <article class="news-item">
+                <a href="<?php the_permalink(); ?>" class="news-thumb">
+                    <?php if (has_post_thumbnail()) : ?>
+                        <?php the_post_thumbnail('medium'); ?>
+                    <?php else : ?>
+                        <img src="<?php echo get_template_directory_uri(); ?>/assets/images/noimage.webp" alt="no image">
+                    <?php endif; ?>
+                </a>
+
+                <div class="news-body">
+                    <div class="news-meta">
+                        <?php
+                        $category = get_the_category();
+                        if ($category) :
+                            $cat_link = get_category_link($category[0]->term_id);
+                        ?>
+                            <a href="<?php echo esc_url($cat_link); ?>" class="news-cat">
+                                <?php echo esc_html($category[0]->name); ?>
+                            </a>
+                        <?php endif; ?>
+                        <time datetime="<?php echo get_the_date('Y-m-d'); ?>" class="news-date">
+                            <?php echo get_the_date('Y.m.d'); ?>
+                        </time>
+                    </div>
+                    
+                    <a href="<?php the_permalink(); ?>">
+                        <h3 class="news-title"><?php the_title(); ?></h3>
+                    </a>
+
+                    <p class="news-excerpt d-none d-md-block">
+                        <?php echo wp_trim_words(get_the_excerpt(), 60, '...'); ?>
+                    </p>
+                </div>
+                
+            </article>
+        <?php endwhile; ?>
     </div>
-</div>
+
+    <?php wp_reset_postdata(); ?>
+<?php else : ?>
+    <p>現在、記事はありません。</p>
+<?php endif; ?>
